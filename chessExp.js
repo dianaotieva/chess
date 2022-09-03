251f71550a1c7de7ad4b041ea769e67df17e0b94
dianaotieva <100135242+dianaotieva@users.noreply.github.com>
2022-09-04 03:25:27 +0400

    Add files via upload

--- chessExp.js
+++ chessExp.js
@@ -1,39 +1,33 @@
-const getData = async(e) =>{
-    e.preventDefault();
-    // console.log(formValues.name);
-    try{
-        const response = await fetch('https://chess-tournament-api.devtest.ge/api/register',{
-            method: 'POST',
-            headers:{
-                'accept': 'application/json',
-                'Content-Type': 'application/json'
+// const getData = async(e) =>{
+//     e.preventDefault();
+//     // console.log(formValues.name);
+//     try{
+//         const response = await fetch('https://chess-tournament-api.devtest.ge/api/register',{
+//             method: 'POST',
+//             headers:{
+//                 'accept': 'application/json',
+//                 'Content-Type': 'application/json'
         
-            },
-            body: JSON.stringify({
-                "name": formValues.name,
-                "email": formValues.email,
-                "phone": formValues.phone,
-                "date_of_birth": "10/20/1997",
-                "experience_level": "begginer",
-                "already_participated": true,
-                "character_id": 2                    
-            })
-        });
-        console.log(response.statusText)
-    }catch(error){
-        console.log(error)
-    }
-}
+//             },
+//             body: JSON.stringify({
+//                 "name": formValues.name,
+//                 "email": formValues.email,
+//                 "phone": formValues.phone,
+//                 "date_of_birth": "10/20/1997",
+//                 "experience_level": "begginer",
+//                 "already_participated": true,
+//                 "character_id": 2                    
+//             })
+//         });
+//         console.log(response.statusText)
+//     }catch(error){
+//         console.log(error)
+//     }
+// }
 
 
-var button = document.querySelector(".next-button");
+// var button = document.querySelector(".next-button");
 
-button.addEventListener("click", getData);
+// button.addEventListener("click", getData);
 
 
-select = document.getElementById("selectKnowledge"); 
-if (select.value) {
-  // value is set to a valid option, so submit form
-  return true;
-}
-return false;
